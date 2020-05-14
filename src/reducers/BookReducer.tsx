5be45ffb67fd1b2db1089cee572164d2af7c2785
DaV888 <dawid888@mail.com>
2020-05-14 22:01:45 +0200

    fix reducer types

--- src/reducers/BookReducer.tsx
+++ src/reducers/BookReducer.tsx
@@ -1,11 +1,34 @@
 import { v4 as uuidv4 } from 'uuid';
 import { bookProp } from '../contexts/BookContext';
 
-export const BookReducer = (state: Array<bookProp>, action: any) => {
+enum BookReducerActions {
+    addBook = 'ADD_BOOK',
+    removeBook = 'REMOVE_BOOK',
+}
+
+// type BookReducerActionType = {
+//     type: 'REMOVE_BOOK', id: book.id
+//     dispatch({ type: 'ADD_BOOK', book: { title, author } });
+// }
+
+type BookReducerActionsType =
+    | {
+          type: BookReducerActions.addBook;
+          book: {
+              title: string;
+              author: string;
+          };
+      }
+    | {
+          type: BookReducerActions.removeBook;
+          id: string;
+      };
+
+export const BookReducer = (state: Array<bookProp>, action: BookReducerActionsType) => {
     switch (action.type) {
-        case 'ADD_BOOK':
+        case BookReducerActions.addBook:
             return [...state, { title: action.book.title, author: action.book.author, id: uuidv4() }];
-        case 'REMOVE_BOOK':
+        case BookReducerActions.removeBook:
             return state.filter((book: bookProp) => book.id !== action.id);
         default:
             return state;
